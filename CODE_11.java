4e9505e0e1c3a323b0105273ce59fb7d23e651a7
Shubham Singh <73664329+shubhamsingh77ss@users.noreply.github.com>
2025-09-17 21:54:50 +0530

    Create CODE_11.java

---
+++ CODE_11.java
@@ -0,0 +1,26 @@
+/*
+1 
+1 2 
+1 2 3 
+1 2 3 4 
+1 2 3 4 5 
+
+*/
+class ss
+{
+    public static void main(String args[])
+    {
+        int n=5;
+        for(int i=0;i<n;i++)
+        {
+            int k=1;
+            for(int j=0;j<i+1;j++)
+            {
+                
+                System.out.print(k+" ");
+                k++;
+            }
+            System.out.println();
+        }
+    }
+}
