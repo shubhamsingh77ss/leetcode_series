69738bb5abdb890a3b472dd4e30f89779611e61a
Shubham Singh <73664329+shubhamsingh77ss@users.noreply.github.com>
2025-09-17 21:47:05 +0530

    Create CODE_10.java

---
+++ CODE_10.java
@@ -0,0 +1,44 @@
+//
+/*pattern
+*  
+**
+***
+****      *
+************
+****      *
+***
+**
+*
+*/
+public class Pattern {
+    public static void main(String[] args) {
+        int n = 5;
+
+        // First loop
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Print middle part
+        System.out.print("*".repeat(n - 1));
+        System.out.print(" ".repeat(n + 1));
+        System.out.println("*");
+
+        System.out.println("*".repeat(2 * n + 2));
+
+        System.out.print("*".repeat(n - 1));
+        System.out.print(" ".repeat(n + 1));
+        System.out.println("*");
+
+        // Last loop
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 2; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
